824f71b97862bd62322a34e7f61f5d306a2fd726
Bidzuweb <handifyshop@gmail.com>
2025-07-31 16:49:57 -0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import HomePage from './HomePage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./HomePage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HomePage />
   </React.StrictMode>
